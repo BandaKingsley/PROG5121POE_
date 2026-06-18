53f4856d5525f4bf239a54ca073718ec3332ae46
BandaKingsley <131707163+BandaKingsley@users.noreply.github.com>
2026-06-18 20:35:47 +0200

    Created and implemented user registration

---
+++ src/main/java/com/mycompany/programming1a_poe/Programming1A_POE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programming1a_poe;
+
+/**
+ *
+ * @author Norothy
+ */
+public class Programming1A_POE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
